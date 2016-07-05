02223760c9c3c36faafd513d2f58065157a7a858
TxsAdamWest <Twinadanz@gmail.com>
2016-07-05 12:43:09 -0500

    api connected

---
+++ src/scripts/AppView.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const AppView = React.createClass({
+
+	render: function() {
+		console.log("Render fired!")
+		return (
+			<div>
+				<h1>Giphy Clonez!!!K11j1</h1>
+			</div>
+			)
+	}
+
+})
+
+
+
+
+
+
+export default AppView
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,15 +1,58 @@
 import React from 'react'
-import ReactDOM from 'react-dom'	
+import ReactDOM from 'react-dom'
 
+import Backbone from 'backbone'
+
+import AppView from './AppView.js'
+
+// parameters note >> http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC 
+
+
+// Start out by creating a collection where we will get all our our api results.
 const app = function() {
 
-	const Header = React.createClass({
-		render: () => {
-			return <h1>YOLO</h1>
-		}
-	})
+    var GifCollection = Backbone.Collection.extend({
+        //The params should be stated here according to what the api requires to make a request.
+        url: 'http://api.giphy.com/v1/gifs/search',
+        _key: 'dc6zaTOxFJmzC',
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
+        parse: function(jsonResp) {
+            return jsonResp.data
+        }
+
+    })
+
+    //The heart of of the app where all changing of views is handled.
+    const GifRouter = Backbone.Router.extend({
+        routes: {
+            'search/:query': '_handleSearch'
+        },
+
+        _handleSearch: function(query) {
+        	//Make a new instance of the collection in order to invoke it.
+    		var gifColl = new GifCollection()
+
+            gifColl.fetch({
+                //Fetch requires some data, otherwise will default to taking JUST whats in url.  (Which is not enough for a request here.)
+                data: {
+                    //Fetch will get url naturally, but also needs our apikey and a q, or query.  We'll throw in a dummy search.	
+                    api_key: gifColl._key,
+                    q: query
+                }
+            })
+            //Since we're using React to render views here, this is where we 'mount' the component.  Basically we are loading in the info that will be rendered.  Note that we aren't using .then here.  With React, we have a virtual DOM always listening for changes, once it hears the change, it will auto render with new data.  Zamn!
+            ReactDOM.render(<AppView collection={gifColl} />, document.querySelector('.container'))
+        
+        },
+
+        initialize: function() {
+        	Backbone.history.start()
+        }
+
+    })
+
+    var rtr = new GifRouter()
+
 }
 
 app()
